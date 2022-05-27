1f9352b37778707d0993d3fdede8b66c01ec69bc
adelquis-molinari <66643930+adelquis-molinari@users.noreply.github.com>
2022-05-27 10:14:59 -0300

    cambios en rout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -12,12 +12,12 @@ const Layout = () => {
                 <h2 className="text-4xl font-black text-center text-white ">CRM - Clientes</h2>
                 <nav className="mt-10">
                     <Link 
-                        className={`${urlActual === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
-                        to="/"
+                        className={`${urlActual === '/clientes/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes/"
                     >Clientes</Link>
                     <Link 
-                         className={`${urlActual === '/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
-                        to="/nuevo"
+                         className={`${urlActual === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes/nuevo"
                     >Nuevo Cliente</Link>
                 </nav>
             </div>
